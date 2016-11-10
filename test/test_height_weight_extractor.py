c992ac8068775a6ac8e2442ac5ca6e4f1a13a7bf
jasonslepicka <jasonslepicka@gmail.com>
2016-11-09 18:20:25 -0800

    Add test case for separating height and weight output into multiple fields

--- test/test_height_weight_extractor.py
+++ test/test_height_weight_extractor.py
@@ -25,6 +25,20 @@ class TestHeightWeightExtractorMethods(unittest.TestCase):
                          'height': [{'foot': 5, 'inch': 5}],
                          'weight': [{'pound': 130}]})
 
+    def test_height_weight_extractor_separated(self):
+        doc = {'content': "\n TS RUBI: THE NAME SAYS IT ALL!  \n INCALL $250 OUTCALL $350 \n \n \n \n \n \n Gender \n Age \n Ethnicity \n Hair Color \n Eye Color \n Height \n Weight \n Measurements \n Affiliation \n Availability \n Available To \n \n \n \n \n Transsexual \n 27 \n Latino/Hispanic \n Brown \n Hazel \n 5'5\" \n 130 lb \n 34C - 28\" - 34\" \n ", 'b': 'world'}
+
+        extractor = HeightWeightExtractor().set_metadata(
+            {'extractor': 'height_weight'})
+        extractor_processor = ExtractorProcessor().set_input_fields(
+            ['content']).set_output_fields(['height', 'weight']).set_extractor(extractor)
+        updated_doc = extractor_processor.extract(doc)
+
+        self.assertEqual(updated_doc['height'][0]['result'][0]['value'],
+                         {'foot': 5, 'inch': 5})
+        self.assertEqual(updated_doc['weight'][0]['result'][0]['value'],
+                         {'pound': 130})
+
 
 if __name__ == '__main__':
     unittest.main()

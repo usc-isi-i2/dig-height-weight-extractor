4291684e0bc35a5d2ca10ce6367288d138214f13
ZwEin <zwein27@gmail.com>
2016-11-11 12:37:41 -0800

    refine output format

--- digHeightWeightExtractor/hw_helper.py
+++ digHeightWeightExtractor/hw_helper.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-07-22 17:52:30
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-11-11 11:16:55
+# @Last Modified time: 2016-11-11 12:37:28
 
 import re
 
@@ -36,6 +36,31 @@ HW_WEIGHT_UNIT_KILOGRAM_ABBRS = ['kg']
 HW_HEIGHT_UNITS_DICT = {
     HW_WEIGHT_UNIT_POUND: HW_WEIGHT_UNIT_POUND_ABBRS,
     HW_WEIGHT_UNIT_KILOGRAM: HW_WEIGHT_UNIT_KILOGRAM_ABBRS
+}
+
+# Transform
+
+HW_TRANSFORM_DICT = {
+    (HW_HEIGHT_UNIT_METER, HW_HEIGHT_UNIT_METER): 1,
+    (HW_HEIGHT_UNIT_METER, HW_HEIGHT_UNIT_CENTIMETER): 100,
+    (HW_HEIGHT_UNIT_METER, HW_HEIGHT_UNIT_FOOT): (1/0.3048),
+    (HW_HEIGHT_UNIT_METER, HW_HEIGHT_UNIT_INCH): (1/0.0254),
+    (HW_HEIGHT_UNIT_CENTIMETER, HW_HEIGHT_UNIT_METER): 0.01,
+    (HW_HEIGHT_UNIT_CENTIMETER, HW_HEIGHT_UNIT_CENTIMETER): 1,
+    (HW_HEIGHT_UNIT_CENTIMETER, HW_HEIGHT_UNIT_FOOT): (1/30.48),
+    (HW_HEIGHT_UNIT_CENTIMETER, HW_HEIGHT_UNIT_INCH): (1/2.54),
+    (HW_HEIGHT_UNIT_FOOT, HW_HEIGHT_UNIT_METER): 0.3048,
+    (HW_HEIGHT_UNIT_FOOT, HW_HEIGHT_UNIT_CENTIMETER): 30.48,
+    (HW_HEIGHT_UNIT_FOOT, HW_HEIGHT_UNIT_FOOT): 1,
+    (HW_HEIGHT_UNIT_FOOT, HW_HEIGHT_UNIT_INCH): 12,
+    (HW_HEIGHT_UNIT_INCH, HW_HEIGHT_UNIT_METER): 0.0254,
+    (HW_HEIGHT_UNIT_INCH, HW_HEIGHT_UNIT_CENTIMETER): 2.54,
+    (HW_HEIGHT_UNIT_INCH, HW_HEIGHT_UNIT_FOOT): (1/12),
+    (HW_HEIGHT_UNIT_INCH, HW_HEIGHT_UNIT_INCH): 1,
+    (HW_WEIGHT_UNIT_POUND, HW_WEIGHT_UNIT_POUND): 1,
+    (HW_WEIGHT_UNIT_POUND, HW_WEIGHT_UNIT_KILOGRAM): 0.45359237,
+    (HW_WEIGHT_UNIT_KILOGRAM, HW_WEIGHT_UNIT_POUND): 2.2046,
+    (HW_WEIGHT_UNIT_KILOGRAM, HW_WEIGHT_UNIT_KILOGRAM): 1,
 }
 
 
@@ -88,16 +113,21 @@ re_ls_weight = re.compile(reg_ls_weight, re.IGNORECASE)
 
 class HWHelper(object):
 
-    def extract_height(self, text):
-        return re_us_height.findall(text) + re_ls_height.findall(text)
-
-    def extract_weight(self, text):
-        return re_us_weight.findall(text) + re_ls_weight.findall(text)
+    ######################################################################
+    #   Clean
+    ######################################################################
 
     def clean_extraction(self, extraction):
         extraction = extraction.replace(':', '')
         extraction = extraction.lower().strip()
         return extraction
+
+    def remove_dups(self, extractions):
+        return [dict(_) for _ in set([tuple(dict_item.items()) for dict_item in extractions if dict_item])]
+
+    ######################################################################
+    #   Normalize
+    ######################################################################
 
     def normalize_height(self, extraction):
         extraction = self.clean_extraction(extraction)
@@ -157,8 +187,44 @@ class HWHelper(object):
 
         return ans
 
-    def remove_dups(self, extractions):
-        return [dict(_) for _ in set([tuple(dict_item.items()) for dict_item in extractions if dict_item])]
+    ######################################################################
+    #   Unit Transform
+    ######################################################################
+    
+    def transform(self, extractions, target_unit):
+        ans = []
+        for extraction in extractions:
+            imd_value = 0.
+            for (unit, value) in extraction.iteritems():
+                imd_value += HW_TRANSFORM_DICT[(unit, target_unit)] * value
+                # print imd_value, HW_TRANSFORM_DICT[(unit, target_unit)], value
+            ans.append(imd_value)
+        return ans
+
+    ######################################################################
+    #   Output Format
+    ######################################################################
+
+    def format_output(self, target_unit, value):
+        if target_unit == HW_HEIGHT_UNIT_FOOT:
+            ft_value = str(value)
+            if '.' in ft_value:
+                left_part, right_part = ft_value.split('.')
+                return '{0}\'{1}"'.format(left_part.strip(), int(12*float('.'+right_part.strip())))
+            else:
+                return ft_value + '\''
+        return value
+    
+
+    ######################################################################
+    #   Main
+    ######################################################################
+
+    def extract_height(self, text):
+        return re_us_height.findall(text) + re_ls_height.findall(text)
+
+    def extract_weight(self, text):
+        return re_us_weight.findall(text) + re_ls_weight.findall(text)
 
     def extract(self, text):
         height_extractions = self.extract_height(text)
@@ -169,9 +235,21 @@ class HWHelper(object):
         weight_extractions = self.remove_dups(
             [self.normalize_weight(_) for _ in weight_extractions])
 
+        print 'height_extractions:', height_extractions
+        print 'weight_extractions:', weight_extractions
+
+        height = {'raw': text}
+        weight = {'raw': text}
+
+        for target_unit in [HW_HEIGHT_UNIT_CENTIMETER, HW_HEIGHT_UNIT_FOOT]:
+            height[target_unit] = [self.format_output(target_unit, _) for _ in self.transform(height_extractions, target_unit)]
+
+        for target_unit in [HW_WEIGHT_UNIT_KILOGRAM, HW_WEIGHT_UNIT_POUND]:
+            weight[target_unit] = [self.format_output(target_unit, _) for _ in self.transform(weight_extractions, target_unit)]
+
         return {
-            'height': height_extractions,
-            'weight': weight_extractions
+            'height': height,
+            'weight': weight
         }
 
 
@@ -185,7 +263,11 @@ if __name__ == '__main__':
 
     # text = "Measurements: 105lbs 5'2\" 34c with a beautiful face"
 
-    text = "Travel: \n worldwide \n \n \n Weight: \n 117 lb (53 kg) \n \n \n Height: \n 5.5 ft (166 cm) \n \n \n Ethnicity: \n Indian \n"
+    # text = "Travel: \n worldwide \n \n \n Weight: \n 117 lb (53 kg) \n \n \n Height: \n 5.5 ft (166 cm) \n \n \n Ethnicity: \n Indian \n"
+
+    # text = "Hair Long Blonde Languages Afrikaans English Body Type slender Age 20-24 Breasts A Eyes blue Height 1.78 Skin Fair Weight 51 Zandalee"
+    
+    text = "Hair Long Blonde Languages Afrikaans English Body Type slender Age 20-24 Breasts A Eyes blue Height 1.78 Skin Fair Weight 51 Zandalee"
 
     import json
     hw = HWHelper()

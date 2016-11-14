c96633d19ff1f11a8059d9a2d10f8dfae441daa3
jasonslepicka <jasonslepicka@gmail.com>
2016-11-13 16:55:02 -0800

    Update version to 0.3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from os import path
 
 setup(
     name = 'digHeightWeightExtractor',
-    version = '0.1.0',
+    version = '0.3.0',
     description = 'digHeightWeightExtractor',
     author = 'Lingzhe Teng',
     author_email = 'zwein27@gmail.com',
